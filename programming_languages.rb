b84920ee7290d47594e84908598f93e229469480
Joshua Denenberg <j.denenberg42@gmail.com>
2017-09-03 19:35:27 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,19 @@
+require "pry"
+
 def reformat_languages(languages)
+  reformatted_languages = {}
+
+  languages.each do |style, language|
+    language.each do |current_lang, data|
+      if reformatted_languages[current_lang]
+        reformatted_languages[current_lang][:style] << style
+      else
+        reformatted_languages[current_lang] = data
+        reformatted_languages[current_lang][:style] = [style]
+      end
+    end
+  end
+
+  return reformatted_languages
   # your code here
 end
